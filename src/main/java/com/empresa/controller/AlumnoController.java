901d39271322c99c22d9507adb727a15793115b1
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-05-23 15:55:23 -0500

    metodo elimina

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -47,7 +49,6 @@ public class AlumnoController {
 	@PutMapping
 	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
 		log.info(">>>> actualiza  " + obj.getIdAlumno());
-		
 		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
 		if (optAlumno.isPresent()) {
 			Alumno objSalida = service.insertaActualizaAlumno(obj);
@@ -57,10 +58,22 @@ public class AlumnoController {
 				return ResponseEntity.badRequest().build();
 			}	
 		}else {
-			log.info(">>>> actualiza  no existe el id : " + obj.getIdAlumno());
+			log.info(">>>> actualiza no existe el id : " + obj.getIdAlumno());
 			return ResponseEntity.badRequest().build();
 		}
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Alumno> elimina(@PathVariable("id") int idAlumno){
+		log.info(">>>> elimina  " + idAlumno);
+		Optional<Alumno> optAlumno = service.obtienePorId(idAlumno);
+		if (optAlumno.isPresent()) {
+			service.eliminaAlumno(idAlumno);
+			return ResponseEntity.ok(optAlumno.get());
+		}else {
+			log.info(">>>> elimina no existe el id : " + idAlumno);
+			return ResponseEntity.badRequest().build();
+		}
+	}
 	
 }
