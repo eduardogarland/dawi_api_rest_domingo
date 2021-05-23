33a30bf47538da61bb7a4f14818ffbe818f260a1
jorgejacinto9701 <jorgejacinto9701@gmail.com>
2021-05-23 15:50:30 -0500

    Metodo actualiza

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,11 +1,13 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +31,7 @@ public class AlumnoController {
 		List<Alumno> lstAlumno = service.listaAlumno();
 		return ResponseEntity.ok(lstAlumno);
 	}
+
 	
 	@PostMapping
 	public ResponseEntity<Alumno> registra(@RequestBody Alumno obj){
@@ -40,5 +43,24 @@ public class AlumnoController {
 			return ResponseEntity.badRequest().build();
 		}
 	}
+	
+	@PutMapping
+	public ResponseEntity<Alumno> actualiza(@RequestBody Alumno obj){
+		log.info(">>>> actualiza  " + obj.getIdAlumno());
+		
+		Optional<Alumno> optAlumno = service.obtienePorId(obj.getIdAlumno());
+		if (optAlumno.isPresent()) {
+			Alumno objSalida = service.insertaActualizaAlumno(obj);
+			if (objSalida != null) {
+				return ResponseEntity.ok(objSalida);
+			}else {
+				return ResponseEntity.badRequest().build();
+			}	
+		}else {
+			log.info(">>>> actualiza  no existe el id : " + obj.getIdAlumno());
+			return ResponseEntity.badRequest().build();
+		}
+	}
+	
 	
 }
